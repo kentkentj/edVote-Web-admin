a7ec6dcd2429f134d25e97bdb7e314f6c9f898ea
kentkentj <kentkentj@gmail.com>
2022-01-02 10:19:04 +0800

    EdVote 0.0.0.0

---
+++ public/School-admin/index.php
@@ -0,0 +1,3 @@
+<?php include 'templates/header.php' ?>
+<?php include 'content-sample.php' ?>
+<?php include 'templates/footer.php' ?>
---
+++ public/School-admin/templates/footer.php
@@ -0,0 +1,8 @@
+<!-- MDB -->
+<script type="text/javascript" src="js/mdb.min.js"></script>
+  <!-- Custom scripts -->
+  <script type="text/javascript" src="js/admin.js"></script>
+
+</body>
+
+</html>
